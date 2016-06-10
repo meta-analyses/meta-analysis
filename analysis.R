46437e0c6aa38c4e9a71e876202a886cd155228a
usr110 <m.aliabbas@gmail.com>
2016-06-10 16:57:34 +0100

    Add code for various outcomes

--- analysis.R
+++ analysis.R
@@ -1,3 +1,4 @@
+rm (list = ls())
 # Read the data
 data <- read.csv("data/09.06_COMBINED DATASET REDUCED.csv", header = T, stringsAsFactors = F, skipNul = TRUE)
 
@@ -6,3 +7,75 @@ source("all-functions.R")
 
 # Identify unique outcomes
 uoutcome <- data.frame(outcome = as.character(unique(data$outcome)))
+
+
+## ALL CAUSE MORTALITY
+
+# Overall All Cause Mortality
+acmdata <- getDiseaseSpecificData(data, uoutcome$outcome[1], paexposure = "LTPA", overall1 = 1)
+acmfdata <- formatData(acmdata, kcases = T)
+metaAnalysis(acmfdata, ptitle = "All Cause Mortality - LTPA - Total Population")
+
+# All Cause Mortality - Males
+acmmdata <- getDiseaseSpecificData(data, uoutcome$outcome[1], paexposure = "LTPA", gender = 1)
+acmmfdata <- formatData(acmmdata, kcases = T)
+metaAnalysis(acmmfdata, ptitle = "All Cause Mortality - LTPA - Males")
+
+# All Cause Mortality - Females
+acmfdata <- getDiseaseSpecificData(data, uoutcome$outcome[1], paexposure = "LTPA", gender = 2)
+acmffdata <- formatData(acmfdata, kcases = T)
+metaAnalysis(acmffdata, ptitle = "All Cause Mortality - LTPA - Females")
+
+## STROKE
+
+# Overall Stroke
+strokedata <- getDiseaseSpecificData(data, "stroke", paexposure = "LTPA", overall1 = 1)
+strokefdata <- formatData(strokedata, kcases = T)
+# Remove all rows with missing RR and Dose
+strokefdata <- subset(strokefdata, !is.na(rr) & !is.na(dose))
+metaAnalysis(strokefdata, ptitle = "Stroke - LTPA - Total Population")
+
+# Stroke Male
+strokemdata <- getDiseaseSpecificData(data, "stroke", paexposure = "LTPA", gender = 1)
+strokemfdata <- formatData(strokemdata, kcases = T)
+# Remove all rows with missing RR and Dose
+strokemfdata <- subset(strokemfdata, !is.na(rr) & !is.na(dose))
+metaAnalysis(strokemfdata, ptitle = "Stroke - LTPA - Males")
+
+# Stroke Female
+strokefdata <- getDiseaseSpecificData(data, "stroke", paexposure = "LTPA", gender = 2)
+strokeffdata <- formatData(strokefdata, kcases = T)
+# Remove all rows with missing RR and Dose
+strokeffdata <- subset(strokeffdata, !is.na(rr) & !is.na(dose))
+# Temporarily remove study #63
+strokeffdata <- subset(strokeffdata, !(ref_number %in% c(63)))
+metaAnalysis(strokeffdata, ptitle = "Stroke - LTPA - Females")
+
+
+## CHD
+
+# Overall CHD
+chddata <- getDiseaseSpecificData(data, "CHD", paexposure = "LTPA", overall1 = 1)
+chdfdata <- formatData(chddata, kcases = T)
+# Remove all rows with missing RR and Dose
+chdfdata <- subset(chdfdata, !is.na(rr) & !is.na(dose))
+metaAnalysis(chdfdata, ptitle = "CHD - LTPA - Total Population")
+
+
+# CHD MALE
+chdmdata <- getDiseaseSpecificData(data, "CHD", paexposure = "LTPA", gender = 1)
+chdmfdata <- formatData(chdmdata, kcases = T)
+# Remove all rows with missing RR and Dose
+chdmfdata <- subset(chdmfdata, !is.na(rr) & !is.na(dose))
+metaAnalysis(chdmfdata, ptitle = "CHD - LTPA - Males")
+
+
+# CHD FEMALE
+chdfdata <- getDiseaseSpecificData(data, "CHD", paexposure = "LTPA", gender = 2)
+chdffdata <- formatData(chdfdata, kcases = T)
+# Remove all rows with missing RR and Dose
+chdffdata <- subset(chdffdata, !is.na(rr) & !is.na(dose))
+# Temporarily remove study #63
+strokeffdata <- subset(strokeffdata, !(ref_number %in% c(63)))
+metaAnalysis(chdffdata, ptitle = "CHD - LTPA - Females")
+
