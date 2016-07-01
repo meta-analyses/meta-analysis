820db0bc841671a16dcaf98466174634741f2998
usr110 <m.aliabbas@gmail.com>
2016-07-01 13:58:12 +0100

    Calculate total persons from follow up and n_baseline. Similarly calculate mean follow up from total persons and n_baseline.

--- analysis.R
+++ analysis.R
@@ -2,6 +2,13 @@ rm (list = ls())
 # Read the data
 data <- read.csv("data/09.06_COMBINED DATASET REDUCED.csv", header = T, stringsAsFactors = F, skipNul = TRUE)
 
+data$tot_personyrs <- as.numeric(data$tot_personyrs)
+data[(is.na(data$tot_personyrs)),]$tot_personyrs <- 
+  data[(is.na(data$tot_personyrs)),]$mean_followup * data[(is.na(data$tot_personyrs)),]$n_baseline
+data[(is.na(data$mean_followup)),]$mean_followup <- 
+  data[(is.na(data$mean_followup)),]$tot_personyrs / data[(is.na(data$mean_followup)),]$n_baseline
+data[(is.na(data$mean_followup)),]$mean_followup <- 
+  data[(is.na(data$mean_followup)),]$tot_personyrs / data[(is.na(data$mean_followup)),]$n_baseline
 # Read all the functions
 source("all-functions.R")
 
@@ -14,17 +21,19 @@ uoutcome <- data.frame(outcome = as.character(unique(data$outcome)))
 # Overall All Cause Mortality
 acmdata <- getDiseaseSpecificData(data, uoutcome$outcome[1], paexposure = "LTPA", overall1 = 1)
 acmfdata <- formatData(acmdata, kcases = T)
-metaAnalysis(acmfdata, ptitle = "All Cause Mortality - LTPA - Total Population")
+metaAnalysis(acmfdata, ptitle = "All Cause Mortality - LTPA - Total Population", covMethed = T)
 
 # All Cause Mortality - Males
 acmmdata <- getDiseaseSpecificData(data, uoutcome$outcome[1], paexposure = "LTPA", gender = 1)
 acmmfdata <- formatData(acmmdata, kcases = T)
+# Remove all rows with missing total persons for RR and OR, and missing person years for HR
+# acmmfdata1 <- subset(acmmfdata, !(effect_measure == "rr" & (!is.na(totalpersons) || totalpersons == 0)))
 metaAnalysis(acmmfdata, ptitle = "All Cause Mortality - LTPA - Males")
 
 # All Cause Mortality - Females
 acmfdata <- getDiseaseSpecificData(data, uoutcome$outcome[1], paexposure = "LTPA", gender = 2)
 acmffdata <- formatData(acmfdata, kcases = T)
-metaAnalysis(acmffdata, ptitle = "All Cause Mortality - LTPA - Females")
+metaAnalysis(acmffdata, ptitle = "All Cause Mortality - LTPA - Females") 
 
 ## ALL CAUSE MORTALITY - TPA
 
@@ -124,16 +133,16 @@ cvdfdata <- formatData(cvddata, kcases = T)
 cvdfdata <- subset(cvdfdata, !is.na(rr) & !is.na(dose))
 metaAnalysis(cvdfdata, ptitle = "CVD - LTPA - Total Population")
 
-# CHD MALE
+# CVD MALE
 cvdmdata <- getDiseaseSpecificData(data, "CVD", paexposure = "LTPA", gender = 1)
 cvdmfdata <- formatData(cvdmdata, kcases = T)
 # Remove all rows with missing RR and Dose
 cvdmfdata <- subset(cvdmfdata, !is.na(rr) & !is.na(dose))
 metaAnalysis(cvdmfdata, ptitle = "CVD - LTPA - Males")
 
-# CHD FEMALE
+# CVD FEMALE
 cvdfdata <- getDiseaseSpecificData(data, "CVD", paexposure = "LTPA", gender = 2)
 cvdffdata <- formatData(cvdfdata, kcases = T)
 # Remove all rows with missing RR and Dose
 cvdffdata <- subset(cvdffdata, !is.na(rr) & !is.na(dose))
-metaAnalysis(chdffdata, ptitle = "CVD - LTPA - Females", covMethed = T)
+metaAnalysis(cvdffdata, ptitle = "CVD - LTPA - Females", covMethed = T)
