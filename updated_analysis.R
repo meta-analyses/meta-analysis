1084277f0ef66e5e62995a184170ffdff18fa7d9
usr110 <m.aliabbas@gmail.com>
2017-06-22 12:49:07 +0100

    Add problematic studies for total population

--- updated_analysis.R
+++ updated_analysis.R
@@ -1,8 +1,8 @@
 rm (list = ls())
 
-total_population <- F
+total_population <- T
 male_population <- F
-female_population <- T
+female_population <- F
 
 # Read the data
 raw_data <- read.csv("data/20170621_MASTER_PA_Dose_Metananalysis_Data_Extraction.csv", header = T, stringsAsFactors = F, skipNul = TRUE)
@@ -33,6 +33,8 @@ source("all-functions.R")
 # Identify unique outcomes
 uoutcome <- data.frame(outcome = as.character(unique(raw_data$outcome)))
 uoutcome$outcome <- as.character(uoutcome$outcome)
+# Sort
+uoutcome$outcome <- sort(uoutcome$outcome)
 
 #Remove empty outcome
 #nv <- uoutcome[!apply(is.na(uoutcome) | uoutcome == "", 1, all),]
@@ -73,15 +75,13 @@ raw_data <- subset(brd, select = c(ref_number, outcome, pa_domain_subgroup, over
 
 if (total_population){
 for (i in 1:nrow(uoutcome)){
-  #i = 1
+  # i = 6
   
   cat("Outcome: ", uoutcome$outcome[i], " and i ", i, "\n")
   acmfdata <- subset(raw_data, outcome == uoutcome$outcome[i] & pa_domain_subgroup == "LTPA" & (overall == 1 | sex_subgroups == 3))
   if (nrow(acmfdata) > 0){
     #acmfdata$dose <- acmfdata$Final.Harmonised.exposure..MMET.hrs.wk.
     #acmfdata$Final.Harmonised.exposure..MMET.hrs.wk. <- NULL
-    
-    #acmfdata$rr <- acmfdata$effect
     
     acmfdata <- getMissingVariables(acmfdata, infertotalpersons = T, kcases = T)
     
@@ -95,29 +95,29 @@ for (i in 1:nrow(uoutcome)){
     
     b <- acmfdata
     
-    acmfdata <- subset(b, select = c(id, ref_number, effect_measure, type, totalpersons, personyrs, dose, rr, logrr, cases, se))
+    acmfdata <- subset(b, select = c(id, ref_number, effect_measure, type, totalpersons, personyrs, dose, rr, logrr, cases, uci_effect, lci_effect, se))
     
-    #metaAnalysis(acmfdata, ptitle = paste( uoutcome$outcome[i] , " LTPA - Total Population"), covMethed = T)
-    
-    #acmfdata <- b
-    
-    if (i == 11)
-      acmfdata <- subset(acmfdata, !id %in% 1)
-    if (i == 1){ #1 4 5 6 7 9 10 11 12 13 14 15 16 17 19 20
-      # 9, 14, 15
-      acmfdata <- subset(b, id %in% c(1, 4, 5, 6, 7, 10:13, 16, 17, 19, 20))# 5 6 7 9 10 11 12 13 14 15 16 17 19 20))
-      #metaAnalysis(acmfdata, ptitle = paste( uoutcome$outcome[i] , " LTPA - Total Population"), covMethed = T)
-    }
-    
-    if (i == 7){#1 2 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
-      # 2, 18
-      acmfdata <- subset(b, id %in% c(1, 4:17, 19, 20))
-      #metaAnalysis(acmfdata, ptitle = paste( uoutcome$outcome[i] , " LTPA - Total Population"), covMethed = T)
+    # if (uoutcome$outcome[i] == 'endometrial cancer')
+    #   acmfdata <- subset(acmfdata, !id %in% 1)
+    if (uoutcome$outcome[i] == 'breast cancer'){ #1 4 5 6 7 9 10 11 12 13 14 15 16 17 19 20
+      
+      
+      acmfdata <- subset(acmfdata, !id %in% c(14))
+      
+      # Set standard error to zero to the first exposure
+      # id == 15, rr 1.05, effect 1.05, uci_effect 1.34, lci_effect 0.82, cases == 210, 
+      acmfdata[acmfdata$id == 15 & acmfdata$rr == 1.05 & acmfdata$cases == 210,]$se <- 0
+      # acmfdata[acmfdata$id == 15 & acmfdata$rr == 1.05 & acmfdata$effect == 1.05 & acmfdata$uci_effect == 1.34 & acmfdata$lci_effect == 0.82
+      #          & acmfdata$cases == 210,]$se <- 0
+      
+      # # -----------------------
+      # # 9, 14, 15
+      # acmfdata <- subset(b, id %in% c(1, 4, 5, 6, 7, 10:13, 16, 17, 19, 20))
       
     }
     
     if (nrow(acmfdata) > 0){
-      metaAnalysis(acmfdata, ptitle = paste( uoutcome$outcome[i] , " LTPA - Total Population"), covMethed = T)
+      metaAnalysis(acmfdata, ptitle = paste( uoutcome$outcome[i] , " LTPA - Total Population"), covMethed = T, minQuantile = 0, maxQuantile = 0.75)
     }
   }
 }
@@ -142,27 +142,27 @@ for (i in 1:nrow(uoutcome)){
     
     
     
-    cat(unique(acmfdata$id))
+    #cat(unique(acmfdata$id))
     
     b <- acmfdata
     
     acmfdata <- subset(b, select = c(id, ref_number, effect_measure, type, totalpersons, personyrs, dose, rr, logrr, cases, se))
     
-    if (i == 4){#1 3 5 6
+    if (uoutcome$outcome[i] == 'total cancer'){#1 3 5 6
       # 1
       acmfdata <- subset(b, !id %in% c(1))
-      metaAnalysis(acmfdata, ptitle = paste( uoutcome$outcome[i] , " LTPA - Total Population"))
+      #metaAnalysis(acmfdata, ptitle = paste( uoutcome$outcome[i] , " LTPA - Total Population"))
       
     }
     
-    if (i == 6){#1 3 5 6
+    if (uoutcome$outcome[i] == 'lung cancer'){#1 3 5 6
       # 1
       acmfdata <- subset(b, !id %in% c(1))
       #metaAnalysis(acmfdata, ptitle = paste( uoutcome$outcome[i] , " LTPA - Total Population"), covMethed = T)
       
     }
     
-    if (i == 7){#1:10
+    if (uoutcome$outcome[i] == 'CVD'){#1:10
       # 7, 9, 10
       acmfdata <- subset(b, id %in% c(1:6, 8))
       #metaAnalysis(acmfdata, ptitle = paste( uoutcome$outcome[i] , " LTPA - Total Population"))
@@ -206,13 +206,13 @@ for (i in 1:nrow(uoutcome)){
     
     acmfdata <- subset(b, select = c(id, ref_number, effect_measure, type, totalpersons, personyrs, dose, rr, logrr, cases, se))
     
-    if (i == 1){ #1 4 5 6 7 9 10 11 12 13 14 15 16 17 19 20
+    if (uoutcome$outcome[i] == 'breast cancer'){ #1 4 5 6 7 9 10 11 12 13 14 15 16 17 19 20
       # 11, 17
       acmfdata <- subset(b, id %in% c(1, 4:7, 9, 10, 12:16, 12, 20))
       #metaAnalysis(acmfdata, ptitle = paste( uoutcome$outcome[i] , " LTPA - Total Population"), covMethed = T)
     }
     
-    if (i == 11){ #1 2 3 4 6 8 9
+    if (uoutcome$outcome[i] == 'endometrial cancer'){ #1 2 3 4 6 8 9
       # 1
       acmfdata <- subset(b, id %in% c(2:4, 6, 8, 9))
       #metaAnalysis(acmfdata, ptitle = paste( uoutcome$outcome[i] , " LTPA - Total Population"), covMethed = T)
