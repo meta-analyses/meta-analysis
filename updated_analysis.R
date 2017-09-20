3b7ccdda1455cdd4358c0d939263e02a6cf7c3fc
usr110 <m.aliabbas@gmail.com>
2017-09-20 16:36:59 +0100

    Identify last knot by using dose and personyrs

--- updated_analysis.R
+++ updated_analysis.R
@@ -6,9 +6,20 @@ female_population <- T
 
 if (total_population){
   for (i in 1:nrow(uoutcome)){
-    #i <- 5
-    cat("Total Population - Outcome: ", uoutcome$outcome[i], " and i ", i, "\n")
+    i <- 11
+    #cat("Total Population - Outcome: ", uoutcome$outcome[i], " and i ", i, "\n")
     acmfdata <- subset(raw_data_tp_ltpa, outcome == uoutcome$outcome[i] & pa_domain_subgroup == local_pa_domain_subgroup)
+    
+    # Filter studies by study size
+    if (i == 1){
+      acmfdata <- subset(acmfdata, n_baseline >= 40000)
+    }else{
+      acmfdata <- subset(acmfdata, n_baseline >= 10000)
+    }
+    
+    local_cov_method <- F
+    if (i == 3 || i == 4) local_cov_method <- T
+      
     if (nrow(acmfdata) > 0){
 
       acmfdata <- getMissingVariables(acmfdata, infertotalpersons = T, kcases = T)
@@ -20,18 +31,51 @@ if (total_population){
       # Subset to selected columns
       acmfdata <- subset(acmfdata, select = c(id, ref_number, Author, effect_measure, type, totalpersons, personyrs, dose, rr, logrr, cases, uci_effect, lci_effect, se))
       
+      top_df <- subset(acmfdata, dose >= (as.numeric(quantile(acmfdata$dose)[4])))
+      bottom_df <- setdiff(acmfdata, top_df)
+      personyrs_pert <- 0.25
+      max_pert <- 0.25
+      
+      continue <- TRUE
+      
+      percentile <- ecdf(acmfdata$dose)
+      
+      while(continue)
+      {
+        # cat(" sum ", sum(top_df$personyrs, na.rm = T), "\n" )
+        if (sum(top_df$totalpersons, na.rm = T) >= (personyrs_pert * sum(acmfdata$totalpersons, na.rm = T)) ){
+          
+          continue <- FALSE
+        }else{
+          max_dose <- max(bottom_df$dose)
+          row_df <- subset(bottom_df, dose == max_dose)
+          # Rmeove the maximum point
+          top_df <- rbind(top_df, row_df)
+          bottom_df <- subset(bottom_df, dose != max_dose)
+          # max_pert <- (min(top_df$dose) / max(acmfdata$dose) * 100)
+          #personyrs_pert <- max(bottom_df$dose) / max(acmfdata$dose)
+          
+          #cat(personyrs_pert, " - ", nrow(top_df), " - ", nrow(bottom_df), "\n")
+          
+        }
+      }
+      
+      
+      cat("Total Population - Outcome: ", uoutcome$outcome[i], " and i ", i, " with quantiles ", percentile(max_dose), "\n")
+      
+      
       #acmfdata <- subset(acmfdata, !is.na(rr))
       
-      #if (i == 5)
-      #  acmfdata <- subset(acmfdata, !is.na(rr))
-      
+      if (i == 5)
+        acmfdata <- subset(acmfdata, !is.na(rr))
       
       if (local_pa_domain_subgroup == "TPA" && (i == 4 || i == 10)){
         acmfdata[acmfdata$logrr == 0,]$se <- acmfdata[acmfdata$logrr == 0,]$uci_effect <- acmfdata[acmfdata$logrr == 0,]$lci_effect <- 0
       }
-      
+
       if (nrow(acmfdata) > 0){
-        metaAnalysis(acmfdata, ptitle = paste0( uoutcome$outcome[i] , " (", local_pa_domain_subgroup,") ", " - Total Population"), maxQuantile = 0.65)
+        metaAnalysis(acmfdata, ptitle = paste0( uoutcome$outcome[i] , " (", local_pa_domain_subgroup,") ", " - Total Population"),
+                     covMethed = local_cov_method, maxQuantile = percentile(max_dose))
       }
     }
   }
