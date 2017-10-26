f599d6d88df45b876ebde68a52dcd9d3057cb8a0
usr110 <m.aliabbas@gmail.com>
2017-10-26 12:28:58 +0100

    Sanity check for last knots

--- sanity_check.R
+++ sanity_check.R
@@ -165,3 +165,248 @@ for (i in 1:nrow(uoutcome)){
   }
 }
 
+
+for (i in 1:nrow(uoutcome)){
+  # i <- 1
+  acmfdata <- subset(raw_data_gsp_ltpa, outcome == uoutcome$outcome[i] & pa_domain_subgroup == local_pa_domain_subgroup)
+  # acmfdata <- getMissingVariables(acmfdata, infertotalpersons = T, kcases = T)
+  if (!is.null(acmfdata) && nrow(acmfdata) > 0){
+    # Remove when totalperson is not available for hr, and personsyears for rr/or
+    #acmfdata <- subset(acmfdata, !((effect_measure == "hr" & (is.na(personyrs) | personyrs == 0) ) |
+    #                                 (effect_measure != "hr" & (is.na(totalpersons | totalpersons == 0) ) ) ))
+    acmfdata$ref_number <- sapply(strsplit(acmfdata$ref_number," "), `[`, 1)
+    m_pop <- filter(acmfdata, sex_subgroups == 1)
+    w_pop <- filter(acmfdata, sex_subgroups == 2)
+    
+    for (j in unique(m_pop$ref_number)){
+      # j <- "13"
+      m_pop_nb <- filter(m_pop, ref_number == j) %>% select(n_baseline) %>% distinct()
+      w_pop_nb <- filter(m_pop, ref_number == j) %>% select(n_baseline) %>% distinct()
+      
+      if ( (m_pop_nb + m_pop_nb) < 1000){
+        cat(uoutcome$outcome[i], " - ", j , " \n")
+      }
+      
+    }
+    
+    # # cat(length(unique(acmfdata$id)), " ", length(unique(acmfdata$Study)), "\n")
+    # for (j in unique(acmfdata$ref_number)){
+    #   td <- filter(acmfdata, ref_number == j)
+    # 
+    #   if (length(unique(td$sex_subgroups)) != 2){
+    #     cat(uoutcome$outcome[i], " with sex group ", unique(td$sex_subgroups), "\n")
+    # 
+    #   }
+    # }
+  }
+}
+
+if (T){
+  for (i in 3){#1:nrow(uoutcome)){
+    for (j in seq(from = 0.75, to = 1, by = 0.01)){
+      # i <- 3
+      # j <- 0.85
+      cat("Total Population - Outcome: ", uoutcome$outcome[i], " and j ", j, "\n")
+      acmfdata <- subset(raw_data_tp_ltpa, outcome == uoutcome$outcome[i] & pa_domain_subgroup == local_pa_domain_subgroup &
+                           outcome_type == "mortality")
+      
+      acmfdata <- subset(acmfdata, n_baseline >= 10000)
+      
+      local_cov_method <- F
+      if (nrow(acmfdata) > 0){
+        acmfdata <- getMissingVariables(acmfdata, infertotalpersons = T, kcases = T)
+        # Remove when totalperson is not available for hr, and personsyears for rr/or
+        acmfdata <- subset(acmfdata, !((effect_measure == "hr" & (is.na(personyrs) | personyrs == 0) ) | 
+                                       (effect_measure != "hr" & (is.na(totalpersons | totalpersons == 0) ) ) ))
+        # Subset to selected columns
+        acmfdata <- subset(acmfdata, select = c(id, ref_number, Author, effect_measure, type, totalpersons, personyrs, dose, rr, logrr, cases, uci_effect, lci_effect, se))
+        last_knot <- get_last_knot(acmfdata, dose_pert = j , personyrs_pert = j)
+        cat("dose_pert and person_years_pert ", last_knot, "\n")
+        last_knot <- last_knot[2]
+        if (nrow(acmfdata) > 0){
+          dataset <- data.frame(metaAnalysis(acmfdata, returnval = T, ptitle = "", covMethed = T, minQuantile = 0, maxQuantile = last_knot))
+          if (nrow(dataset) > 0){
+            colnames(dataset) <- c("dose","RR", "lb", "ub")
+            q <- quantile(dataset$dose, c(0, last_knot / 2, last_knot))
+            
+            plotTitle <- paste0( uoutcome$outcome[i] ,  " (", local_pa_domain_subgroup,") ", " - Total Population")
+            
+            plotTitle <-  paste0(simpleCap(plotTitle), ' \n Number of samples: ', 
+                                 length(unique(acmfdata$id)), 
+                                 ' \n Number of people: ' , round(sum(acmfdata$totalpersons)), " ", j)
+            
+            cat(plotTitle, "\n")
+            
+            # xlab = "Marginal MET hours per week" 
+            # print(
+            #   ggplot(dataset, aes(dose, RR)) +
+            #   geom_line(data = dataset) +
+            #   geom_ribbon(data = dataset, aes(ymin=`lb`,ymax=`ub`),alpha=0.4) +
+            #   scale_x_continuous(expand = c(0, 0)) + scale_y_continuous(expand = c(0, 0)) +
+            #   xlab(paste("\n", xlab, "\n")) +
+            #   ylab("\nRelative Risk\n") +
+            #   geom_vline(xintercept= q, linetype="dotted", alpha=0.4) +
+            #   labs(title = paste(plotTitle))
+            # )
+          }
+        }
+      }
+      
+    }
+  }
+}
+
+## 0.77 for CHD total population with outcome_type = mortality
+
+
+
+
+for (g in c(1,2)){# male and female
+  for (i in 9){#stroke
+    for (j in seq(from = 0.75, to = 1, by = 0.01)){
+      gender <- "Male"
+      if (g == 2)
+        gender <- "Female"
+      
+      cat(gender, " Population - Outcome: ", uoutcome$outcome[i], " and j ", j, "\n")
+      acmfdata <- subset(raw_data_gsp_ltpa, 
+                         outcome == uoutcome$outcome[i] & 
+                         pa_domain_subgroup == "LTPA" & 
+                         sex_subgroups == g)
+      
+      local_cov_method <- F
+      if (nrow(acmfdata) > 0){
+        acmfdata <- getMissingVariables(acmfdata, infertotalpersons = T, kcases = T)
+        # Remove when totalperson is not available for hr, and personsyears for rr/or
+        acmfdata <- subset(acmfdata, !((effect_measure == "hr" & (is.na(personyrs) | personyrs == 0) ) | 
+                                         (effect_measure != "hr" & (is.na(totalpersons | totalpersons == 0) ) ) ))
+        # Subset to selected columns
+        acmfdata <- subset(acmfdata, select = c(id, ref_number, Author, effect_measure, type, totalpersons, personyrs, dose, rr, logrr, cases, uci_effect, lci_effect, se))
+        last_knot <- get_last_knot(acmfdata, dose_pert = j , personyrs_pert = j)
+        cat("dose_pert and person_years_pert ", last_knot, "\n")
+        last_knot <- last_knot[2]
+        if (nrow(acmfdata) > 0){
+          dataset <- data.frame(metaAnalysis(acmfdata, returnval = T, ptitle = "", covMethed = T, minQuantile = 0, maxQuantile = last_knot))
+          if (nrow(dataset) > 0){
+            colnames(dataset) <- c("dose","RR", "lb", "ub")
+            q <- quantile(dataset$dose, c(0, last_knot / 2, last_knot))
+            gender <- "Male"
+            if (g == 2)
+              gender <- "Female"
+            
+            plotTitle <- paste0( uoutcome$outcome[i] ,  " (", local_pa_domain_subgroup,") ", " - ", gender, "Population")
+            
+            plotTitle <-  paste0(simpleCap(plotTitle), ' \n Number of samples: ', 
+                                 length(unique(acmfdata$id)), 
+                                 ' \n Number of people: ' , round(sum(acmfdata$totalpersons)), " ", j)
+            
+            cat(plotTitle, "\n")
+            
+            # xlab = "Marginal MET hours per week" 
+            # print(
+            #   ggplot(dataset, aes(dose, RR)) +
+            #   geom_line(data = dataset) +
+            #   geom_ribbon(data = dataset, aes(ymin=`lb`,ymax=`ub`),alpha=0.4) +
+            #   scale_x_continuous(expand = c(0, 0)) + scale_y_continuous(expand = c(0, 0)) +
+            #   xlab(paste("\n", xlab, "\n")) +
+            #   ylab("\nRelative Risk\n") +
+            #   geom_vline(xintercept= q, linetype="dotted", alpha=0.4) +
+            #   labs(title = paste(plotTitle))
+            # )
+          }
+        }
+      }
+      
+    }
+  }
+}
+
+# .83 for male population and .85 for female population
+
+
+
+for (g in c(2)){# female
+  for (ut in c("all", "incidence")){
+    for (j in seq(from = 0.75, to = 1, by = 0.01)){
+      i <- 4 #colon cancer
+      gender <- "Male"
+      if (g == 2)
+        gender <- "Female"
+      cat(gender, " Population - Outcome: ", uoutcome$outcome[i], " type ", ut , " and j ", j, "\n")
+      if (ut == "all"){
+        
+        
+        acmfdata <- subset(raw_data_gsp_ltpa, 
+                           outcome == uoutcome$outcome[i] & 
+                             pa_domain_subgroup == "LTPA" & 
+                             sex_subgroups == g)
+        
+        
+      }else{
+        
+        acmfdata <- subset(raw_data_gsp_ltpa, 
+                           outcome == uoutcome$outcome[i] & 
+                             pa_domain_subgroup == "LTPA" & 
+                             sex_subgroups == g &
+                             outcome_type == "incidence")
+        
+        
+      }
+      
+      local_cov_method <- F
+      if (nrow(acmfdata) > 0){
+        acmfdata <- getMissingVariables(acmfdata, infertotalpersons = T, kcases = T)
+        # Remove when totalperson is not available for hr, and personsyears for rr/or
+        acmfdata <- subset(acmfdata, !((effect_measure == "hr" & (is.na(personyrs) | personyrs == 0) ) | 
+                                         (effect_measure != "hr" & (is.na(totalpersons | totalpersons == 0) ) ) ))
+        # Subset to selected columns
+        acmfdata <- subset(acmfdata, select = c(id, ref_number, Author, effect_measure, type, totalpersons, personyrs, dose, rr, logrr, cases, uci_effect, lci_effect, se))
+        last_knot <- get_last_knot(acmfdata, dose_pert = j , personyrs_pert = j)
+        
+        last_knot <- last_knot[2]
+        if (nrow(acmfdata) > 0){
+          dataset <- data.frame(metaAnalysis(acmfdata, returnval = T, ptitle = "", covMethed = T, minQuantile = 0, maxQuantile = last_knot))
+          if (nrow(dataset) > 0){
+            colnames(dataset) <- c("dose","RR", "lb", "ub")
+            q <- quantile(dataset$dose, c(0, last_knot / 2, last_knot))
+            gender <- "Male"
+            if (g == 2)
+              gender <- "Female"
+            
+            plotTitle <- paste0( uoutcome$outcome[i] ,  " (", local_pa_domain_subgroup,") ", " - ", gender, "Population")
+            
+            plotTitle <-  paste0(simpleCap(plotTitle), ' \n Number of samples: ', 
+                                 length(unique(acmfdata$id)), 
+                                 ' \n Number of people: ' , round(sum(acmfdata$totalpersons)), " ", j)
+            
+            # cat("dose_pert and person_years_pert ", last_knot, "\n")
+            
+            # cat(plotTitle, "\n")
+            
+            # xlab = "Marginal MET hours per week" 
+            # print(
+            #   ggplot(dataset, aes(dose, RR)) +
+            #   geom_line(data = dataset) +
+            #   geom_ribbon(data = dataset, aes(ymin=`lb`,ymax=`ub`),alpha=0.4) +
+            #   scale_x_continuous(expand = c(0, 0)) + scale_y_continuous(expand = c(0, 0)) +
+            #   xlab(paste("\n", xlab, "\n")) +
+            #   ylab("\nRelative Risk\n") +
+            #   geom_vline(xintercept= q, linetype="dotted", alpha=0.4) +
+            #   labs(title = paste(plotTitle))
+            # )
+          }
+        }
+      }
+      
+    }
+  }
+}
+
+# Female  Population - Outcome:  colon cancer  type  all  and j  0.78 
+# Female  Population - Outcome:  colon cancer  type  incidence  and j  0.78
+
+
+
+
+
+
