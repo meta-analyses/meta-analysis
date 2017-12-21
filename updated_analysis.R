c5bcc9ec3351dd0af7bc3d88307042e41f5462f1
usr110 <m.aliabbas@gmail.com>
2017-12-21 15:02:38 +0000

    Replace rr with RR

--- updated_analysis.R
+++ updated_analysis.R
@@ -6,18 +6,18 @@ female_population <- F
 local_last_knot <- 0.75
 
 if (total_population){
-  for (i in 1:1){#nrow(uoutcome)){
-    # i <- 1
+  for (i in 1:nrow(uoutcome)){
+    i <- 1
     cat("Total Population - Outcome: ", uoutcome$outcome[i], " and i ", i, "\n")
-    acmfdata <- subset(raw_data_tp_ltpa, outcome == uoutcome$outcome[i] & pa_domain_subgroup == local_pa_domain_subgroup) # &
-    # outcome_type == "mortality")
+    acmfdata <- subset(raw_data_tp_ltpa, outcome == uoutcome$outcome[i] & pa_domain_subgroup == local_pa_domain_subgroup & outcome_type == "mortality")
+    acmfdata <- subset(acmfdata, n_baseline >= 10000)
     local_cov_method <- F
     if (i == 3 || i == 4) local_cov_method <- T
     if (nrow(acmfdata) > 0){
       acmfdata <- getMissingVariables(acmfdata, infertotalpersons = T, kcases = T)
       acmfdata <- subset(acmfdata, !((effect_measure == "hr" & (is.na(personyrs) | personyrs == 0) ) | 
                                        (effect_measure != "hr" & (is.na(totalpersons | totalpersons == 0) ) ) ))
-      acmfdata <- subset(acmfdata, select = c(id, ref_number, Author, effect_measure, type, totalpersons, personyrs, dose, rr, logrr, cases, uci_effect, lci_effect, se))
+      acmfdata <- subset(acmfdata, select = c(id, ref_number, Author, effect_measure, type, totalpersons, personyrs, dose, RR, logrr, cases, uci_effect, lci_effect, se))
       last_knot <- get_last_knot(acmfdata, dose_pert = local_last_knot , personyrs_pert = local_last_knot)
       # cat("dose_pert and person_years_pert ", last_knot, "\n")
       last_knot <- last_knot[2]
@@ -28,30 +28,40 @@ if (total_population){
           dataset$personyrs <- round(dataset$personyrs)
           group_by(dataset, id) %>% select(dose, se) %>%
             summarise(min = min(dose), max = max(dose), ref = dose[is.na(se)])
-          obj <- metaAnalysis(acmfdata, returnval = T, ptitle = "", covMethed = T, minQuantile = 0, maxQuantile = last_knot, lout = 1000)
-          dataset2 <- data.frame(dose = obj[1], RR = as.data.frame(obj[2])[1], lb = as.data.frame(obj[2])[2], ub = as.data.frame(obj[2])[3])
+          pa <- acmfdata
+          
+          dataset2 <- data.frame(metaAnalysis(dataset, ptitle = "", returnval = T, covMethed = local_cov_method, minQuantile = 0, maxQuantile = last_knot, lout = 1000))
           colnames(dataset2) <- c("dose","RR", "lb", "ub")
-          plotTitle <- paste0( uoutcome$outcome[i] ,  " (", local_pa_domain_subgroup,") ", " - Total Population")
-          plotTitle <-  paste0(simpleCap(plotTitle), ' \n Number of samples: ',
+          
+          #obj <- metaAnalysis(acmfdata, returnval = T, ptitle = "", covMethed = T, minQuantile = 0, maxQuantile = last_knot, lout = 1000)
+          #dataset2 <- data.frame(dose = obj[1], RR = as.data.frame(obj[2])[1], lb = as.data.frame(obj[2])[2], ub = as.data.frame(obj[2])[3])
+          # colnames(dataset2) <- c("dose","RR", "lb", "ub")
+          plotTitle <- paste0( uoutcome$outcome[i] ,  " - Mortality - Total Population")
+          plotTitle <-  paste0(simpleCap(plotTitle), ' \nNumber of entries: ',
                                length(unique(acmfdata$id)),
-                               ' \n Number of people: ' , round(sum(acmfdata$totalpersons)))#, " ", local_last_knot)
+                               ' \nNumber of people: ' , round(sum(acmfdata$totalpersons)))#, " ", local_last_knot)
           p <- ggplot() +
-            geom_line(data = dataset, aes(dose, rr, col = factor(ref_number), label = personyrs)) +
-            geom_point(data = dataset, aes(dose, rr, col = factor(ref_number)), size = 4 * (dataset$personyrs - min(dataset$personyrs))/diff(range(dataset$personyrs))) +
+            geom_line(data = dataset, aes(dose, RR, col = factor(ref_number), label = personyrs)) +
+            geom_point(data = dataset, aes(dose, RR, col = factor(ref_number)), size = 4 * (dataset$personyrs - min(dataset$personyrs))/diff(range(dataset$personyrs))) +
             geom_line(data = subset(dataset2, dose < as.numeric(q[3])), aes(x = dose, y = RR)) +
             geom_line(data = subset(dataset2, dose >= as.numeric(q[3])), aes(x = dose, y = RR), linetype = "dashed") +
             geom_ribbon(data = subset(dataset2, dose < as.numeric(q[3])), aes(x = dose, ymin=`lb`,ymax=`ub`), alpha = 0.25) +
             geom_ribbon(data = subset(dataset2, dose >= as.numeric(q[3])), aes(x = dose, ymin=`lb`,ymax=`ub`), alpha = 0.10) +
             geom_vline(xintercept= q, linetype="dotted", alpha = 0.6) +
-            scale_x_continuous(expand = c(0, 0)) + scale_y_continuous(expand = c(0, 0)) +
-            coord_cartesian(ylim = c(min(dataset$rr) - 0.2, max(dataset$rr) + 0.2), 
-                            xlim = c(0, max(dataset$dose) + 3)) + 
-            theme(legend.position="none") +
+            scale_x_continuous(expand = c(0, 0),
+                               breaks = seq(from = 0, to = 80, by = 10)) + 
+            scale_y_continuous(expand = c(0, 0),
+                               breaks = seq(from = 0, to = max(dataset2$ub), by = 0.2),
+                               limits = c(0, NA)) +
+            # coord_cartesian(ylim = c(min(dataset$RR) - 0.2, max(dataset$RR) + 0.2), 
+            #                 xlim = c(0, max(dataset$dose) + 3)) + 
+            theme(legend.position="none",
+                  plot.title = element_text(hjust = 0.5)) +
             xlab("\nMarginal MET hours per week\n") +
             ylab("\nRelative Risk\n") +
             labs(title = paste(plotTitle))
           print(p)
-          ggsave(paste0(uoutcome$outcome[i], ".png"), height=5, width=10, units='in', dpi=600, scale = 1.2)
+          ggsave(paste0(uoutcome$outcome[i], "-mortality", ".png"), height=5, width=10, units='in', dpi=600, scale = 1)
         }
       }
     }
@@ -74,9 +84,9 @@ if(male_population){
                                        (effect_measure != "hr" & (is.na(totalpersons | totalpersons == 0) ) ) ))
       
       # Remove where both dose and response are null
-      mdata <- subset(mdata, !is.na(rr) & !is.na(dose))
+      mdata <- subset(mdata, !is.na(RR) & !is.na(dose))
       
-      mdata <- subset(mdata, select = c(id, ref_number, effect_measure, type, totalpersons, personyrs, dose, rr, logrr, cases, uci_effect, lci_effect, se))
+      mdata <- subset(mdata, select = c(id, ref_number, effect_measure, type, totalpersons, personyrs, dose, RR, logrr, cases, uci_effect, lci_effect, se))
       
       m_last_knot <- get_last_knot(mdata, dose_pert = local_last_knot, personyrs_pert = local_last_knot)
       cat("dose_pert and person_years_pert ", m_last_knot, "\n")
@@ -100,9 +110,9 @@ if(male_population){
                                  (effect_measure != "hr" & (is.na(totalpersons | totalpersons == 0) ) ) ))
       
       # Remove where both dose and response are null
-      wdata <- subset(wdata, !is.na(rr) & !is.na(dose))
+      wdata <- subset(wdata, !is.na(RR) & !is.na(dose))
       
-      wdata <- subset(wdata, select = c(id, ref_number, effect_measure, type, totalpersons, personyrs, dose, rr, logrr, cases, uci_effect, lci_effect, se))
+      wdata <- subset(wdata, select = c(id, ref_number, effect_measure, type, totalpersons, personyrs, dose, RR, logrr, cases, uci_effect, lci_effect, se))
       
       w_last_knot <- get_last_knot(wdata, dose_pert = local_last_knot, personyrs_pert = local_last_knot)
       cat("dose_pert and person_years_pert ", w_last_knot, "\n")
@@ -188,9 +198,9 @@ if(female_population){
                                        (effect_measure != "hr" & (is.na(totalpersons | totalpersons == 0) ) ) ))
       
       # Remove where both dose and response are null
-      acmfdata <- subset(acmfdata, !is.na(rr) & !is.na(dose))
+      acmfdata <- subset(acmfdata, !is.na(RR) & !is.na(dose))
       
-      acmfdata <- subset(acmfdata, select = c(id, ref_number, effect_measure, type, totalpersons, personyrs, dose, rr, logrr, cases, uci_effect, lci_effect, se))
+      acmfdata <- subset(acmfdata, select = c(id, ref_number, effect_measure, type, totalpersons, personyrs, dose, RR, logrr, cases, uci_effect, lci_effect, se))
       
       last_knot <- get_last_knot(acmfdata, dose_pert = local_last_knot, personyrs_pert = local_last_knot)
       cat("dose_pert and person_years_pert ", last_knot, "\n")
