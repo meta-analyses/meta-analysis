9eb9ab4711163fd7c85ab613540ba244596a0b4a
usr110 <m.aliabbas@gmail.com>
2018-03-23 11:14:04 +0000

    Filter data for gender specific groups

--- filter_studies.R
+++ filter_studies.R
@@ -1,9 +1,13 @@
 library(tidyverse)
 
 source("init.R")
+
+### Remove studies with less than 40k for all-cause, or 10k for the rest outcomes
+#raw_data <- subset(raw_data, (outcome == "all-cause mortality" & n_baseline >= 40000 ) |
+#                             (outcome != "all-cause mortality" & n_baseline >= 10000 ))
+
 raw_data_tp_ltpa <- subset(raw_data, pa_domain_subgroup == "LTPA")
 for (i in 1:nrow(uoutcome)){
-  #i <- 5
   dat <- subset(raw_data_tp_ltpa, outcome == uoutcome$outcome[i])
   for (j in 1:length(unique(dat$ref_number))){
     dat1 <- subset(dat, ref_number == (unique(dat$ref_number))[j]) 
@@ -12,6 +16,7 @@ for (i in 1:nrow(uoutcome)){
     if (length(usexgroups) > 2 && length(uoverall) > 1){
       # Remove gender specific from total_population
       raw_data_tp_ltpa <- subset(raw_data_tp_ltpa, (ref_number == unique(dat1$ref_number) & !sex_subgroups %in% c(1,2)) | (ref_number != unique(dat1$ref_number))) 
+      # cat(uoutcome$outcome[i], " - ", unique(dat1$ref_number), " - ", unique(dat1$overall), " - ", unique(dat1$sex_subgroups), "\n")
     }
     
   }
@@ -20,14 +25,16 @@ for (i in 1:nrow(uoutcome)){
 raw_data_gsp_ltpa <- subset(raw_data, pa_domain_subgroup == "LTPA" & (sex_subgroups %in% c(1,2)))
 
 for (i in 1:nrow(uoutcome)){
-  if (!i %in% c(2,6)){
+  if (!uoutcome$outcome[i] %in% c('Breast cancer','Endometrial cancer')){
     dat <- subset(raw_data_gsp_ltpa, outcome == uoutcome$outcome[i])
-    for (j in 1:length(unique(dat$ref_number))){
-      dat1 <- subset(dat, ref_number == (unique(dat$ref_number))[j]) 
+    uid <- unique(dat$ref_number)
+    for (j in 1:length(uid)){
+      dat1 <- subset(dat, ref_number == uid[j]) 
       usexgroups <- unique(dat1$sex_subgroups)
       if (length(usexgroups) == 1){
         # Remove single gender specific studies
-        raw_data_gsp_ltpa <- subset(raw_data_gsp_ltpa,(ref_number != unique(dat1$ref_number))) 
+        raw_data_gsp_ltpa <- subset(raw_data_gsp_ltpa, (ref_number != uid[j])) 
+        # cat("gsp ",uoutcome$outcome[i], " - ", unique(dat1$ref_number), " - ", unique(dat1$overall), " - ", unique(dat1$sex_subgroups), "\n")
       }
     }
   }
@@ -42,7 +49,7 @@ raw_data_tp_ltpa[raw_data_tp_ltpa$overall != 1 & raw_data_tp_ltpa$sex_subgroups 
 raw_data_tp_ltpa <- plyr::arrange(raw_data_tp_ltpa, outcome)
 
 
-raw_data_gsp_ltpa[raw_data_gsp_ltpa$sex_subgroups == 1,]$ref_number <- paste(raw_data_gsp_ltpa[raw_data_gsp_ltpa$sex_subgroups == 1,]$ref_number, "-1")
-raw_data_gsp_ltpa[raw_data_gsp_ltpa$sex_subgroups == 2,]$ref_number <- paste(raw_data_gsp_ltpa[raw_data_gsp_ltpa$sex_subgroups == 2,]$ref_number, "-2")
+raw_data_gsp_ltpa[raw_data_gsp_ltpa$sex_subgroups == 1,]$ref_number <- paste0(raw_data_gsp_ltpa[raw_data_gsp_ltpa$sex_subgroups == 1,]$ref_number, "-1")
+raw_data_gsp_ltpa[raw_data_gsp_ltpa$sex_subgroups == 2,]$ref_number <- paste0(raw_data_gsp_ltpa[raw_data_gsp_ltpa$sex_subgroups == 2,]$ref_number, "-2")
 
 raw_data_gsp_ltpa <- plyr::arrange(raw_data_gsp_ltpa, outcome)
