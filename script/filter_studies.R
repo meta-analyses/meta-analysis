f6a74026af18545144361bf27db938d1bc4ab4e0
Ali Abbas <m.aliabbas@gmail.com>
2021-01-28 17:29:15 +0000

    Just use overall column to determine total population

--- script/filter_studies.R
+++ script/filter_studies.R
@@ -4,20 +4,20 @@ source("script/init.R")
 #raw_data <- subset(raw_data, (outcome == "all-cause mortality" & n_baseline >= 40000 ) |
                         #    (outcome != "all-cause mortality" & n_baseline >= 10000 ))
 
-raw_data_tp_ltpa <- subset(raw_data, pa_domain_subgroup == "LTPA")
-for (i in 1:nrow(uoutcome)){
-  dat <- subset(raw_data_tp_ltpa, outcome == uoutcome$outcome[i])
-  for (j in 1:length(unique(dat$ref_number))){
-    dat1 <- subset(dat, ref_number == (unique(dat$ref_number))[j]) 
-    uoverall <- unique(dat1$overall)
-    usexgroups <- unique(dat1$sex_subgroups)
-    if (length(usexgroups) > 2 && length(uoverall) > 1){
-      # Remove gender specific from total_population
-      raw_data_tp_ltpa <- subset(raw_data_tp_ltpa, (ref_number == unique(dat1$ref_number) & !sex_subgroups %in% c(1,2)) | (ref_number != unique(dat1$ref_number))) 
-    }
-    
-  }
-}
+raw_data_tp_ltpa <- subset(raw_data, pa_domain_subgroup == "LTPA" & overall == 1)
+# for (i in 1:nrow(uoutcome)){
+#   dat <- subset(raw_data_tp_ltpa, outcome == uoutcome$outcome[i])
+#   for (j in 1:length(unique(dat$ref_number))){
+#     dat1 <- subset(dat, ref_number == (unique(dat$ref_number))[j]) 
+#     uoverall <- unique(dat1$overall)
+#     usexgroups <- unique(dat1$sex_subgroups)
+#     if (length(usexgroups) > 2 && length(uoverall) > 1){
+#       # Remove gender specific from total_population
+#       raw_data_tp_ltpa <- subset(raw_data_tp_ltpa, (ref_number == unique(dat1$ref_number) & !sex_subgroups %in% c(1,2)) | (ref_number != unique(dat1$ref_number))) 
+#     }
+#     
+#   }
+# }
 
 raw_data_gsp_ltpa <- subset(raw_data, pa_domain_subgroup == "LTPA" & (sex_subgroups %in% c(1,2)))
 
@@ -39,7 +39,9 @@ for (i in 1:nrow(uoutcome)){
 
 ## Create ref_number for men and women subgroups
 ## for total population
-raw_data_tp_ltpa[raw_data_tp_ltpa$overall != 1 & raw_data_tp_ltpa$sex_subgroups == 1,]$ref_number <- paste(raw_data_tp_ltpa[raw_data_tp_ltpa$overall != 1 & raw_data_tp_ltpa$sex_subgroups == 1,]$ref_number, "-1")
+if (nrow(raw_data_tp_ltpa[raw_data_tp_ltpa$overall != 1 & raw_data_tp_ltpa$sex_subgroups == 1,]) > 0)
+  raw_data_tp_ltpa[raw_data_tp_ltpa$overall != 1 & raw_data_tp_ltpa$sex_subgroups == 1,]$ref_number <- paste(raw_data_tp_ltpa[raw_data_tp_ltpa$overall != 1 & raw_data_tp_ltpa$sex_subgroups == 1,]$ref_number, "-1")
+if (nrow(raw_data_tp_ltpa[raw_data_tp_ltpa$overall != 1 & raw_data_tp_ltpa$sex_subgroups == 2,]) > 0)
 raw_data_tp_ltpa[raw_data_tp_ltpa$overall != 1 & raw_data_tp_ltpa$sex_subgroups == 2,]$ref_number <- paste(raw_data_tp_ltpa[raw_data_tp_ltpa$overall != 1 & raw_data_tp_ltpa$sex_subgroups == 2,]$ref_number, "-2")
 
 # Create ID column
