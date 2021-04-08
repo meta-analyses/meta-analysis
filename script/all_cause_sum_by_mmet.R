2e6a99132fe1446fd3e117ac2607ee53c058bc3f
Ali Abbas <m.aliabbas@gmail.com>
2021-04-08 13:18:45 +0100

    Add script to fit and draw sum of total persons by dose

---
+++ script/all_cause_sum_by_mmet.R
@@ -0,0 +1,45 @@
+rm (list = ls())
+library(MASS)
+library(tidyverse)
+library(janitor)
+set.seed(101)
+
+acm <- readr::read_csv('data/csv/all-cause mortality.csv')
+
+my_data <- acm$dose
+my_data[my_data == 0] <- 0.5
+
+
+fit <- fitdistr(my_data, densfun="log-normal")  # we assume my_data ~ Normal(?,?)
+fit
+
+hist(my_data, pch=20, breaks=25, prob=TRUE, main="")
+curve(dlnorm(x, fit$estimate[1], fit$estimate[2]), col="red", lwd=2, add=T)
+
+# For the study with 3 levels of exposure:
+#   
+# 0
+# 0-6 MET.hrs/week
+# 6+ MET.hrs/week
+
+acm %>% filter(dose == 0) %>% dplyr::select(totalpersons) %>% colSums(na.rm = T)
+
+acm %>% filter(dose <= 6) %>% dplyr::select(totalpersons) %>% colSums(na.rm = T)
+
+acm %>% filter(dose > 6) %>% dplyr::select(totalpersons) %>% colSums(na.rm = T)
+
+# For the study with 4 levels of exposure:
+#   
+# 0-5 met.hrs/week
+# 5-9 met.hrs/week
+# 9-20 met.hrs/week
+# 20+ met.hrs/week
+
+acm %>% filter(dose == 0 & dose <= 5) %>% dplyr::select(totalpersons) %>% colSums(na.rm = T)
+
+acm %>% filter(dose > 5 & dose <= 9) %>% dplyr::select(totalpersons) %>% colSums(na.rm = T)
+
+acm %>% filter(dose > 9 & dose <= 20) %>% dplyr::select(totalpersons) %>% colSums(na.rm = T)
+
+acm %>% filter(dose > 20) %>% dplyr::select(totalpersons) %>% colSums(na.rm = T)
+
