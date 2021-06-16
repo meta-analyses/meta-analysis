bc7d4592b2eb13c0bb42add11fe4deb696d809bd
Ali Abbas <m.aliabbas@gmail.com>
2021-06-16 17:14:37 +0100

    Add code to delete missing entries file

--- script/master_script.R
+++ script/master_script.R
@@ -1,6 +1,16 @@
 # Remove all elements from the memory
 rm (list = ls())
 
+# Set log file
+record_removed_entries <- "missing_entries.csv"
+
+# Check its existence
+if (file.exists(record_removed_entries)) {
+  # Delete file if it exists
+  file.remove(record_removed_entries)
+}
+
+
 for(local_last_knot in c(0.75, 0.85))
   for(ALT in c(TRUE, FALSE))
     for (NO_BMI_EFFECT in c(TRUE, FALSE)){
