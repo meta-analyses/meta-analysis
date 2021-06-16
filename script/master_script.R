5678b308be62fabf59ebd52e9618113badb9e539
Ali Abbas <m.aliabbas@gmail.com>
2021-06-16 16:12:16 +0100

    Add gender analysis

--- script/master_script.R
+++ script/master_script.R
@@ -1,10 +1,12 @@
 # Remove all elements from the memory
 rm (list = ls())
 
-for(local_last_knot in c(0.85, 0.75))
+for(local_last_knot in c(0.75, 0.85))
   for(ALT in c(TRUE, FALSE))
     for (NO_BMI_EFFECT in c(TRUE, FALSE)){
+      print(paste(local_last_knot, ALT, NO_BMI_EFFECT, sep = "-"))
       source("script/analysis.R")
+      source("script/gender_analysis.R")
       
     }
 
