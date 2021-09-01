e1fb3c04aa41b6a49db0552e640e12d8a3739a73
Ali Abbas <m.aliabbas@gmail.com>
2021-09-01 12:26:28 +0100

    Add code to load datasets and libraries to produce distr table

--- script/draw_table_dose_distr.R
+++ script/draw_table_dose_distr.R
@@ -1,5 +1,13 @@
+## Initialize global vars
+local_last_knot <- 0.75
+ALT <- FALSE
+NO_BMI_EFFECT <- FALSE
+
+# Initialize datasets
+source("script/filter_studies.R")
+
+# Load additional library
 library(broom)
-library(dplyr)
 
 raw_data_gsp_ltpa %>% 
   mutate(sex = case_when(sex_subgroups == 2 ~ 'female', 
@@ -8,4 +16,4 @@ raw_data_gsp_ltpa %>%
   map_dfr(.x = c(0, 0.025, .05, 0.25, .5, .75, 0.95, 0.975, 1),
           .f = ~ data_frame(Quantile = .x,
                             Value = quantile(x, probs = .x)))
-  })
+  }) %>% readr::write_csv('data/csv/distr/dose_distr_by_sex.csv')
