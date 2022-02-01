f9f0a1f4667f937dc47e9664eb5d3506b8c33e92
Ali Abbas <m.aliabbas@gmail.com>
2022-02-01 10:45:43 +0000

    Reorder plots

--- script/plot-depression-outcomes.R
+++ script/plot-depression-outcomes.R
@@ -40,6 +40,25 @@ source("script/filter_studies.R")
 # Select specific outcomes
 uoutcome_depression <- uoutcome %>% filter(outcome %in% c("Depression", "Elevated depressive symptoms", "Major depression"))
 
+plot_RR <- function(dataset, q, plot_title){
+  
+  # Create plot
+  p <- ggplot() +
+    geom_line(data = subset(dataset, dose < as.numeric(q)), aes(x = dose, y = RR)) +
+    geom_line(data = subset(dataset, dose >= as.numeric(q)), aes(x = dose, y = RR), linetype = "dashed") +
+    geom_ribbon(data = subset(dataset, dose < as.numeric(q)), aes(x = dose, ymin = `lb`, ymax = `ub`), alpha = 0.25) +
+    geom_ribbon(data = subset(dataset, dose >= as.numeric(q)), aes(x = dose, ymin = `lb`, ymax = `ub`), alpha = 0.10) +
+    coord_fixed(ylim = c(0, 1.5), x = c(0, 40), ratio = 10) +
+    theme(
+      legend.position = "none",
+      plot.title = element_text(hjust = 0.5, size = 9)
+    ) +
+    xlab("\nMarginal MET hours per week\n") +
+    ylab("\nRelative Risk\n") + 
+    labs(title = plot_title)
+  p
+}
+
 # Read total population's RR
 prefix_path <- "data/csv/MA-DR/"
 
@@ -49,6 +68,7 @@ df_list <- list()
 last_knot_df <- list()
 # Loop all three last knot position
 for(local_last_knot in c(0.75, 0.85, 0.95)){
+  last_knot_df[[paste0("lastknot-", local_last_knot)]] <- read_csv(paste0(prefix_path, local_last_knot,"/", "75p_diseases.csv"))
   # Loop for outcomes
   for (i in 1:nrow(uoutcome_depression)){
     # Loop through all three outcome types
@@ -68,11 +88,20 @@ for(local_last_knot in c(0.75, 0.85, 0.95)){
       ma_filename <- paste0(snake_case_outcome, "-", snake_case_outcome_type)
       
       fname <- print(paste0(prefix_path, local_last_knot,"/", ma_filename, ".csv"))
-      if (file.exists(fname))
+      if (file.exists(fname)){
         df_list[[paste0(ma_filename, "-last-knot-", local_last_knot)]] <- read_csv(fname)
+        
+        plot_title <- paste0(uoutcome_depression$outcome[i], " - ", simpleCap(dir_name))
+        q <- last_knot_df[[paste0("lastknot-", local_last_knot)]] %>% filter(disease == snake_case_outcome) %>% 
+          dplyr::select(snake_case_outcome_type) %>% 
+          as.numeric() %>% round(1)
+        
+        print(plot_RR(df_list[[paste0(ma_filename, "-last-knot-", local_last_knot)]], q, plot_title))
+        
+      }
+      
     }
   }
-  last_knot_df[[paste0("lastknot-", local_last_knot)]] <- read_csv(paste0("data/csv/MA-DR/", local_last_knot,"/", "75p_diseases.csv"))
 }
 
 dataset <- df_list$`major-depression-fatal-and-non-fatal-last-knot-0.75`
@@ -80,24 +109,7 @@ dataset <- df_list$`major-depression-fatal-and-non-fatal-last-knot-0.75`
 q <- last_knot_df$`lastknot-0.75` %>% filter(disease == "major-depression") %>% dplyr::select("fatal-and-non-fatal") %>% as.numeric() %>%
   round(1)
 
-plot_RR <- function(dataset, q){
-  
-  # Create plot
-  p <- ggplot() +
-    geom_line(data = subset(dataset, dose < as.numeric(q)), aes(x = dose, y = RR)) +
-    geom_line(data = subset(dataset, dose >= as.numeric(q)), aes(x = dose, y = RR), linetype = "dashed") +
-    geom_ribbon(data = subset(dataset, dose < as.numeric(q)), aes(x = dose, ymin = `lb`, ymax = `ub`), alpha = 0.25) +
-    geom_ribbon(data = subset(dataset, dose >= as.numeric(q)), aes(x = dose, ymin = `lb`, ymax = `ub`), alpha = 0.10) +
-    coord_fixed(ylim = c(0, 1.5), x = c(0, 40), ratio = 10) +
-    theme(
-      legend.position = "none",
-      plot.title = element_text(hjust = 0.5, size = 9)
-    ) +
-    xlab("\nMarginal MET hours per week\n") +
-    ylab("\nRelative Risk\n")
-  
-  p
-}
+
 
 plot_RR(dataset, q)
 
